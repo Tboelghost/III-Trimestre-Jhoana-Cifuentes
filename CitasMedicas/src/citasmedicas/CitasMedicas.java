9571985841c9c610e30a635e033d3c6257853ace
Daniel Stiben Niño Fernandez <72568390+Tboelghost@users.noreply.github.com>
2020-11-16 20:13:04 -0500

    Add files via upload
    VERSION 3

---
+++ CitasMedicas/src/citasmedicas/CitasMedicas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citasmedicas;
+
+/**
+ *
+ * @author Daniel-2002
+ */
+public class CitasMedicas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+            Ventana.main(args);
+    }
+    
+}
